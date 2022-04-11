20207cfb062d934457130ca300e153fd335de408
wairimu.Kanene <mercykanene254@gmail.com>
2022-04-11 12:12:20 +0300

    commit4-modified arrays

--- quote-app/src/app/quote.ts
+++ quote-app/src/app/quote.ts
@@ -3,16 +3,14 @@ export class Quote {
   quote:string;
   author:string;
   name:string;
-  submissionDate:string;
   upvotes:number;
   downvotes:number;
-  constructor(id:number,quote:string,author:string,name:string,submissionDate:Date,upvotes:number,downvotes:number)
+  constructor(id:number,quote:string,author:string,name:string,upvotes:number,downvotes:number,){
   this.id=id;
   this.quote=quote;
   this.author=author;
   this.name=name;
-  this.submissionDate=submissionDate;
   this.upvotes=upvotes;
   this.downvotes=downvotes;
 }
-
+}
--- quote-app/src/app/quotes/quotes.component.ts
+++ quote-app/src/app/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Quote} from '/quote';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -9,12 +9,12 @@ import {Quote} from '/quote';
 export class QuotesComponent implements OnInit {
   //quotes array
   quotes:Quote[] =[
-    new Quote(1,'Dont stop Until you are proud','Myle Cyrus','Wairimu Kanene',new Date(2022,2,25)0,0),
-    new Quote(2,'Always be kind because everyone is fighting battles that we dont know about','Oprah Winfred','Margaret Mbaire',new Date(2021,5,7)1,2),
-    new Quote(3,'Music is better than people because it always stays when everyone leaves','Justin Bieber','Brian Muchiri'),new Date(2022,3,29)3,4),
-    new Quote(4,'And when the sunrise we try again','Mandela Ndiba','Nancy Naisoi')new Date(2022,4,5)4,5),
-    new Quote(5,'Life is hard you always have to choose a struggle.Pick a poison','Michael Todd','Sophia Kerubo')new Date(2022,5,19)6,7),
-    new Quote(6,'Good things are coming be happy and stay happy','Annet Smith','Rita Njuguna')new Date(2022,6,8)8,9),
+    new Quote(1,'Dont stop Until you are proud','Myle Cyrus','Wairimu Kanene',0,1),
+    new Quote(2,'Always be kind because everyone is fighting battles that we dont know about','Oprah Winfred','Margaret Mbaire',2,3),
+    new Quote(3,'Music is better than people because it always stays when everyone leaves','Justin Bieber','Brian Muchiri',4,5),
+    new Quote(4,'And when the sunrise we try again','Mandela Ndiba','Nancy Naisoi',5,6),
+    new Quote(5,'Life is hard you always have to choose a struggle.Pick a poison','Michael Todd','Sophia Kerubo',7,8),
+    new Quote(6,'Good things are coming be happy and stay happy','Annet Smith','Rita Njuguna',9,1),
 
   ]
 
