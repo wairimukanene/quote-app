2418c808b70044626187910f656e691685c1c1ff
wairimu.Kanene <mercykanene254@gmail.com>
2022-04-12 08:15:01 +0300

    commit19-modified quote.ts

--- quote-app/src/app/quote.ts
+++ quote-app/src/app/quote.ts
@@ -3,6 +3,5 @@ export class Quote {
   
   constructor(public id:number, public quote:string, public author:string,public name:string,public postedDate:Date, public thumbsUp:number,   public thumbsDown:number)  {
     this.showDetails=false;
-
   }
 }
